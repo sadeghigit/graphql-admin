5829654563672926ea76cb3fc70f285c23d2cdba
Hamed <sadeghigit@gmail.com>
2022-11-13 11:07:01 +0330

    install antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
+import { Button } from 'antd';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Button>Ok</Button>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
